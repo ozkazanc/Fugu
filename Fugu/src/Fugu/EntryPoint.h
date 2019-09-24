c57be61cf64a2629fcfcb231eccd53ecc6c1869d
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-09-24 10:59:03 -0400

    Used spdlog library to make a simple logger class

--- Fugu/src/Fugu/EntryPoint.h
+++ Fugu/src/Fugu/EntryPoint.h
@@ -5,6 +5,14 @@
 extern Fugu::Application* Fugu::CreateApplication();
 
 int main(int argc, char** argv) {
+	Fugu::Log::Init();
+	FG_CORE_TRACE("App, test = {0}, line: {1}", 8, __LINE__);
+	FG_INFO("info: file: {0}", __FILE__);
+	FG_CORE_WARN("Fugu");
+	FG_CORE_ERROR("Engine");
+	FG_CORE_CRITICAL("Assert");
+
+
 	Fugu::Application* app = Fugu::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Fugu/src/Fugu/Log.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Fugu {
+
+	class FUGU_API Log {
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+// Core log macros
+#define FG_CORE_TRACE(...)      ::Fugu::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define FG_CORE_INFO(...)       ::Fugu::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define FG_CORE_WARN(...)       ::Fugu::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define FG_CORE_ERROR(...)      ::Fugu::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define FG_CORE_CRITICAL(...)   ::Fugu::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+// Client log macros
+#define FG_TRACE(...)           ::Fugu::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define FG_INFO(...)            ::Fugu::Log::GetClientLogger()->info(__VA_ARGS__)
+#define FG_WARN(...)            ::Fugu::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define FG_ERROR(...)           ::Fugu::Log::GetClientLogger()->error(__VA_ARGS__)
+#define FG_CRITICAL(...)        ::Fugu::Log::GetClientLogger()->critical(__VA_ARGS__)
