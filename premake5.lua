c4b219ae4a72b87b848fb599da42b549368d2d30
ozkazanc <doruk.ozkazanc@mail.utoronto.ca>
2019-09-30 16:21:06 -0400

    Turned static runtime options off

--- premake5.lua
+++ premake5.lua
@@ -25,6 +25,7 @@ project "Fugu"
 	location "Fugu"
 	kind "SharedLib"
 	language "C++"
+	staticruntime "off"
 	
 	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
@@ -57,7 +58,6 @@ project "Fugu"
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
 		
 		defines
@@ -74,23 +74,24 @@ project "Fugu"
 
 	filter "configurations:Debug"
 		defines "FG_DEBUG"
-		buildoptions "/MDd"
+		runtime "Debug"
 		symbols "On"
 	
 	filter "configurations:Release"
 		defines "FG_RELEASE"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
 	
 	filter "configurations:Dist"
 		defines "FG_DIST"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
 
 project "Sandbox"	
 	location "Sandbox"
 	kind "ConsoleApp"
 	language "C++"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
@@ -114,7 +115,6 @@ project "Sandbox"
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
 		
 		defines
@@ -124,15 +124,15 @@ project "Sandbox"
 
 	filter "configurations:Debug"
 		defines "FG_DEBUG"
-		buildoptions "/MDd"
+		runtime "Debug"
 		symbols "On"
 	
 	filter "configurations:Release"
 		defines "FG_RELEASE"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
 	
 	filter "configurations:Dist"
 		defines "FG_DIST"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
